f2efed8391afa5cd2b842909fa5d2e0adf9bafa6
aabizardd <abizard@student.telkomuniversity.ac.id>
2021-04-26 19:34:05 +0700

    update

--- database baru/kos2 (3).sql
+++ database baru/kos2 (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2021 at 09:26 AM
+-- Generation Time: Apr 26, 2021 at 02:33 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.3.27
 
@@ -80,8 +80,7 @@ CREATE TABLE `kamar` (
 --
 
 INSERT INTO `kamar` (`id_kamar`, `kode_kamar`, `kode_kos`, `harga`, `harga_smesteran`, `deskripsi`, `foto`, `status`, `tgl_tersedia`) VALUES
-(2, 'A12', 'ejah3655', 12000000, 7000000, 'mantap', '1_2fpfv8Np1AGdmp2axA9rXQ.png', 'Tersedia', '2021-03-02'),
-(3, 'A13', 'ejah3655', 10000000, 6000000, 'Large', '1_2fpfv8Np1AGdmp2axA9rXQ.png', 'Tersedia', '2021-03-02');
+(8, '1', 'OSAB6456', 12000000, 6000000, 'asdsa', 'BACKGROUND_MUBES_HMDSI.png', 'Booked', '2021-04-08');
 
 -- --------------------------------------------------------
 
@@ -106,7 +105,7 @@ CREATE TABLE `kosan` (
 --
 
 INSERT INTO `kosan` (`kode_kos`, `nama_kos`, `alamat`, `deskripsi`, `foto`, `jenis_kosan`, `saldo_kos`, `tanggal_daftar`, `id_pemilik`) VALUES
-('ejah3655', 'sejahtera', 'Bogor', 'nyaman sejahter', '1_2fpfv8Np1AGdmp2axA9rXQ.png', 'Putra', 300000, '2021-04-01', 4);
+('OSAB6456', 'KOSABC', 'Bandung', 'Luas kamar ini adalah 12. Untuk tegangan listrik yaitu 12. Deskrip lainnya berupa, 12', 'thumb-1920-1125041.png', 'Putra', 0, '2021-04-08', 9);
 
 -- --------------------------------------------------------
 
@@ -119,30 +118,16 @@ CREATE TABLE `notifikasi` (
   `isi_pesan` varchar(250) NOT NULL,
   `dari` varchar(250) NOT NULL,
   `untuk` varchar(250) NOT NULL,
-  `status_baca` int(11) NOT NULL,
-  `id_pemilik` int(11) NOT NULL,
-  `id_pencari` int(11) NOT NULL
+  `status_baca` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `notifikasi`
 --
 
-INSERT INTO `notifikasi` (`id_notifikasi`, `isi_pesan`, `dari`, `untuk`, `status_baca`, `id_pemilik`, `id_pencari`) VALUES
-(1, 'Ada Pesanan Baru Nih!', '2', '4', 1, 4, 2),
-(2, 'Ada Pesanan Baru Nih!', '2', '4', 1, 4, 2),
-(3, 'Ada Pesanan Baru Nih!', '2', '4', 1, 4, 2),
-(4, 'Ada Pesanan Baru Nih!', '2', '4', 1, 4, 2),
-(5, 'Ada Pesanan Baru Nih!', '2', '4', 1, 4, 2),
-(6, 'Ada Pesanan Baru Nih!', '2', '4', 1, 4, 2),
-(7, 'Pesanan anda diterima!', '4', '2', 0, 4, 2),
-(8, 'Pesanan anda diterima!', '4', '2', 0, 4, 2),
-(9, 'Pesanan anda diterima!', '4', '2', 0, 4, 2),
-(10, 'Pesanan anda diterima!', '4', '2', 0, 4, 2),
-(11, 'Ada Pesanan Baru Nih!', '2', '4', 1, 4, 2),
-(12, 'Ada Pesanan Baru Nih!', '2', '4', 1, 4, 2),
-(13, 'Pesanan anda diterima!', '4', '2', 0, 4, 2),
-(14, 'Pesanan anda diterima!', '4', '2', 0, 4, 2);
+INSERT INTO `notifikasi` (`id_notifikasi`, `isi_pesan`, `dari`, `untuk`, `status_baca`) VALUES
+(2, 'Ada pesanan baru nih!', '27', '26', 0),
+(3, 'Ada pesanan baru nih!', '27', '26', 0);
 
 -- --------------------------------------------------------
 
@@ -159,13 +144,6 @@ CREATE TABLE `pelunasan` (
   `id_pesan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Dumping data for table `pelunasan`
---
-
-INSERT INTO `pelunasan` (`id_lunas`, `tanggal`, `jam_pelunasan`, `jumlah_pelunasan`, `bukti_pelunasan`, `id_pesan`) VALUES
-(9, '2021-03-29', '10:11:02', 8000000, '60642e965ef23.jpg', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -175,7 +153,9 @@ INSERT INTO `pelunasan` (`id_lunas`, `tanggal`, `jam_pelunasan`, `jumlah_pelunas
 CREATE TABLE `pemesanan` (
   `id_pesan` int(10) NOT NULL,
   `nama_penghuni` varchar(250) COLLATE utf8_bin NOT NULL,
-  `tanggal_pesan` date NOT NULL,
+  `nomor_ktp` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `nomor_hp` varchar(15) COLLATE utf8_bin DEFAULT NULL,
+  `tanggal_pesan` date NOT NULL DEFAULT current_timestamp(),
   `tanggal_masuk` date NOT NULL,
   `tanggal_keluar` date NOT NULL,
   `jam` varchar(255) COLLATE utf8_bin NOT NULL,
@@ -184,6 +164,7 @@ CREATE TABLE `pemesanan` (
   `bukti_bayar` varchar(250) COLLATE utf8_bin NOT NULL,
   `sisa_pembayaran` int(255) DEFAULT NULL,
   `status_transaksi` tinyint(255) DEFAULT NULL,
+  `keterangan_pembatalan` text COLLATE utf8_bin DEFAULT NULL,
   `id_pencari` int(255) DEFAULT NULL,
   `id_penghuni` int(11) DEFAULT NULL,
   `id_kamar` int(255) DEFAULT NULL
@@ -193,9 +174,8 @@ CREATE TABLE `pemesanan` (
 -- Dumping data for table `pemesanan`
 --
 
-INSERT INTO `pemesanan` (`id_pesan`, `nama_penghuni`, `tanggal_pesan`, `tanggal_masuk`, `tanggal_keluar`, `jam`, `jangka_waktu`, `jumlah_dp`, `bukti_bayar`, `sisa_pembayaran`, `status_transaksi`, `id_pencari`, `id_penghuni`, `id_kamar`) VALUES
-(1, 'Suci', '2021-03-31', '2021-03-31', '2022-03-31', '', '1 Tahun', '2000000', '6063f7059db65.jpg', 0, 2, 2, NULL, 3),
-(4, 'Lisa', '2021-03-31', '2021-04-01', '2021-10-01', '', '6 Bulan', '300000', '606429f43fa02.jpg', 0, 1, 2, NULL, 2);
+INSERT INTO `pemesanan` (`id_pesan`, `nama_penghuni`, `nomor_ktp`, `nomor_hp`, `tanggal_pesan`, `tanggal_masuk`, `tanggal_keluar`, `jam`, `jangka_waktu`, `jumlah_dp`, `bukti_bayar`, `sisa_pembayaran`, `status_transaksi`, `keterangan_pembatalan`, `id_pencari`, `id_penghuni`, `id_kamar`) VALUES
+(30, 'asdsa', '12312321', '213213', '2021-04-26', '2021-04-26', '2021-10-26', '', '6 Bulan', '1200000', '6086a8669ea4f.png', 4800000, 0, NULL, 4, NULL, 8);
 
 -- --------------------------------------------------------
 
@@ -223,7 +203,8 @@ CREATE TABLE `pemilik_kos` (
 --
 
 INSERT INTO `pemilik_kos` (`id_pemilik`, `nama_pemilik`, `no_telp`, `email`, `jenis_kelamin`, `foto`, `id_user`, `no_ktp`, `no_rek`, `bank`, `atas_nama_rek`, `tgl_daftar`) VALUES
-(4, 'SURAM (Suci Rara Mantap)', '081300000001', 'm.abizard1123@gmail.com', 'Laki-laki', '1_2fpfv8Np1AGdmp2axA9rXQ.png', 18, '000101010101', '12341234123', 'Mandiri', 'Suci', '2021-04-01');
+(9, 'Dadang Konelo', '13231232', 'ani@gmail.com', 'Laki-laki', 'thumb-1920-1125041.png', 26, '1232131232', '12321321321', 'Mandiri', 'Dadang Konelo', '2021-04-08'),
+(11, 'Pencari CInta Sejati', '123123213', 'm.abizard1123@gmail.com', 'Laki-laki', 'Capture.PNG', 28, '123123123', '112321321', 'BCA', 'Jeks', '2021-04-21');
 
 -- --------------------------------------------------------
 
@@ -252,7 +233,7 @@ CREATE TABLE `pencari_kos` (
 --
 
 INSERT INTO `pencari_kos` (`id_pencari`, `nama_pencari`, `tempat_lahir`, `tgl_lahir`, `jenis_kelamin`, `email`, `no_telp`, `no_ktp`, `status`, `no_telp_wali`, `foto`, `tgl_daftar`, `id_user`) VALUES
-(2, 'pencari', 'entah', '2021-12-31', 'Laki-laki', 'pencari@gmail.com', '123412341234', '123412341234', 'warga', '123412341234', '1_2fpfv8Np1AGdmp2axA9rXQ.png', '2021-04-01', 15);
+(4, 'Pencari Kos abadai', 'Bandung', '2021-04-09', 'Laki-laki', 'ani@gmail.com', '1232132321', '12321321', '123232', '12312321321', 'thumb-1920-1125041.png', '2021-04-08', 27);
 
 -- --------------------------------------------------------
 
@@ -294,31 +275,6 @@ CREATE TABLE `tmp_forget_pass` (
   `exp` varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tmp_forget_pass`
---
-
-INSERT INTO `tmp_forget_pass` (`id`, `email`, `kode`, `exp`) VALUES
-(3, 'pemilik@gmail.com', '43b52842663fa6e4100c72751d08de6f', '22'),
-(2, 'pemilik@gmail.com', 'f0a5944c6a815b7bb44323d783eede4d', '22'),
-(4, 'pemilik@gmail.com', 'f24ad6f72d6cc4cb51464f2b29ab69d3', '22'),
-(5, 'pemilik@gmail.com', '4bb236de7787ceedafdff83bb8ea4710', '22'),
-(6, 'pemilik@gmail.com', '5f0453f78909173a7ce2eb874d2a7f52', '22'),
-(7, 'pemilik@gmail.com', 'b112ca4087d668785e947a57493d1740', '22'),
-(8, 'pemilik@gmail.com', 'c2c2a04512b35d13102459f8784f1a2d', '22'),
-(9, 'pemilik@gmail.com', '243facb29564e7b448834a7c9d901201', '22'),
-(10, 'pemilik@gmail.com', '95a7e4252fc7bc562a711ef96884a383', '22'),
-(11, 'pemilik@gmail.com', '4f1f29888cabf5d45f866fe457737a23', '22'),
-(12, 'pemilik@gmail.com', '4764f37856fc727f70b666b8d0c4ab7a', '22'),
-(13, 'pemilik@gmail.com', 'ab9ebd57177b5106ad7879f0896685d4', '21'),
-(14, 'pemilik@gmail.com', 'c30fb4dc55d801fc7473840b5b161dfa', '21'),
-(15, 'pemilik@gmail.com', 'dbbf603ff0e99629dda5d75b6f75f966', '22'),
-(16, 'pencari@gmail.com', '5747a0021eb349e9c8d3667cf1a5e9ec', '11'),
-(17, 'pemilik@gmail.com', '941e1aaaba585b952b62c14a3a175a61', '12'),
-(18, 'pemilik@gmail.com', '298f587406c914fad5373bb689300433', '12'),
-(19, 'pencari@gmail.com', '1d01bd2e16f57892f0954902899f0692', '07'),
-(20, 'admin@gmail.com', '931af583573227f0220bc568c65ce104', '07');
-
 -- --------------------------------------------------------
 
 --
@@ -339,9 +295,10 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `is_pemilik`, `is_admin`, `status_aktif_pemilik`) VALUES
-(15, 'pencari', '827ccb0eea8a706c4c34a16891f84e7b', 0, 0, 0),
 (17, 'admin', '827ccb0eea8a706c4c34a16891f84e7b', 0, 1, 0),
-(18, 'pemilik', 'fcea920f7412b5da7be0cf42b8c93759', 1, 0, 1);
+(26, 'pemilik', '827ccb0eea8a706c4c34a16891f84e7b', 1, 0, 1),
+(27, 'pencari', '827ccb0eea8a706c4c34a16891f84e7b', 0, 0, 0),
+(28, 'pemilik2', '827ccb0eea8a706c4c34a16891f84e7b', 1, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -476,49 +433,49 @@ ALTER TABLE `bayar_di_muka`
 -- AUTO_INCREMENT for table `kamar`
 --
 ALTER TABLE `kamar`
-  MODIFY `id_kamar` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_kamar` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `notifikasi`
 --
 ALTER TABLE `notifikasi`
-  MODIFY `id_notifikasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id_notifikasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `pelunasan`
 --
 ALTER TABLE `pelunasan`
-  MODIFY `id_lunas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_lunas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `pemesanan`
 --
 ALTER TABLE `pemesanan`
-  MODIFY `id_pesan` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_pesan` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `pemilik_kos`
 --
 ALTER TABLE `pemilik_kos`
-  MODIFY `id_pemilik` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_pemilik` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `pencari_kos`
 --
 ALTER TABLE `pencari_kos`
-  MODIFY `id_pencari` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_pencari` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tmp_forget_pass`
 --
 ALTER TABLE `tmp_forget_pass`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_user` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `user_token`
@@ -558,9 +515,9 @@ ALTER TABLE `kosan`
 -- Constraints for table `pemesanan`
 --
 ALTER TABLE `pemesanan`
-  ADD CONSTRAINT `pemesanan_ibfk_2` FOREIGN KEY (`id_pencari`) REFERENCES `pencari_kos` (`id_pencari`),
+  ADD CONSTRAINT `pemesanan_ibfk_2` FOREIGN KEY (`id_pencari`) REFERENCES `pencari_kos` (`id_pencari`) ON DELETE CASCADE,
   ADD CONSTRAINT `pemesanan_ibfk_3` FOREIGN KEY (`id_penghuni`) REFERENCES `penghuni_kos` (`nik`) ON DELETE CASCADE,
-  ADD CONSTRAINT `pemesanan_ibfk_4` FOREIGN KEY (`id_kamar`) REFERENCES `kamar` (`id_kamar`);
+  ADD CONSTRAINT `pemesanan_ibfk_4` FOREIGN KEY (`id_kamar`) REFERENCES `kamar` (`id_kamar`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `pemilik_kos`
@@ -579,6 +536,8 @@ DELIMITER $$
 -- Events
 --
 CREATE DEFINER=`root`@`localhost` EVENT `update_pemesanan_status` ON SCHEDULE EVERY 1 MINUTE STARTS '2021-03-30 13:41:12' ON COMPLETION NOT PRESERVE ENABLE DO UPDATE pemesanan set status_transaksi = 3 WHERE datediff(current_date(), tanggal_pesan) > 0$$
+
+CREATE DEFINER=`root`@`localhost` EVENT `cancle_pesanan` ON SCHEDULE EVERY 10 HOUR STARTS '2021-04-22 01:03:21' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM pemesanan WHERE DATEDIFF(NOW(),tanggal_pesan) >=2$$
 
 DELIMITER ;
 COMMIT;
