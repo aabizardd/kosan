835e836a8d663f7fc8a85d23ad692d271bce05e2
aabizardd <abizard@student.telkomuniversity.ac.id>
2021-07-08 01:54:07 +0700

    upd

--- application/views/pencari/header_pencari.php
+++ application/views/pencari/header_pencari.php
@@ -1,63 +1,83 @@
 <!-- Content Wrapper -->
-<?php error_reporting(0); ?>
+<?php error_reporting(0);?>
 <div id="content-wrapper" class="d-flex flex-column">
 
-  <!-- Main Content -->
-  <div id="content">
-
-    <!-- Topbar -->
-    <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-      <!-- Sidebar Toggle (Topbar) -->
-      <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-        <i class="fa fa-bars"></i>
-      </button>
-
-      <!-- Topbar Search -->
+    <!-- Main Content -->
+    <div id="content">
 
 
-      <script>
-        var url = document.URL
-        // var searchbox =
-        var last_url = url.substr(url.lastIndexOf('/') + 1)
-        if (last_url == 'pembayaran') {
-          document.getElementById("search_for").style.display = "none";
 
-        }
-      </script>
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-      <!-- Topbar Navbar -->
-      <ul class="navbar-nav ml-auto">
+            <!-- Sidebar Toggle (Topbar) -->
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                <i class="fa fa-bars"></i>
+            </button>
 
-        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-        <li class="nav-item dropdown no-arrow d-sm-none">
-          <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-search fa-fw"></i>
-          </a>
-          <!-- Dropdown - Messages -->
-          <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-            <form class="form-inline mr-auto w-100 navbar-search">
-              <div class="input-group">
-                <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                <div class="input-group-append">
-                  <button class="btn btn-primary" type="button">
-                    <i class="fas fa-search fa-sm"></i>
-                  </button>
+            <!-- Topbar Search -->
+
+            <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                <div class="input-group">
+                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                        aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" type="button" style="background-color: #5F9EA0;">
+                            <i class="fas fa-search fa-sm"></i>
+                        </button>
+                    </div>
                 </div>
-              </div>
             </form>
-          </div>
-        </li>
 
-        <!-- Nav Item - Alerts -->
-        <!--<li class="nav-item dropdown no-arrow mx-1">
+
+            <script>
+            var url = document.URL
+            // var searchbox =
+            var last_url = url.substr(url.lastIndexOf('/') + 1)
+            if (last_url == 'pembayaran') {
+                document.getElementById("search_for").style.display = "none";
+
+            }
+            </script>
+
+            <!-- Topbar Navbar -->
+            <ul class="navbar-nav ml-auto">
+
+
+
+
+                <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+                <li class="nav-item dropdown no-arrow d-sm-none">
+                    <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-search fa-fw"></i>
+                    </a>
+                    <!-- Dropdown - Messages -->
+                    <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
+                        aria-labelledby="searchDropdown">
+                        <form class="form-inline mr-auto w-100 navbar-search">
+                            <div class="input-group">
+                                <input type="text" class="form-control bg-light border-0 small"
+                                    placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                                <div class="input-group-append">
+                                    <button class="btn btn-primary" type="button">
+                                        <i class="fas fa-search fa-sm"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </li>
+
+                <!-- Nav Item - Alerts -->
+                <!--<li class="nav-item dropdown no-arrow mx-1">
               <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-bell fa-fw"></i>
                 <!- Counter - Alerts -->
-        <!--<span class="badge badge-danger badge-counter">3+</span>
+                <!--<span class="badge badge-danger badge-counter">3+</span>
               </a>-->
-        <!-- Dropdown - Alerts -->
-        <!--<div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
+                <!-- Dropdown - Alerts -->
+                <!--<div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
                 <h6 class="dropdown-header">
                   Alerts Center
                 </h6>
@@ -98,85 +118,91 @@
               </div>
             </li>-->
 
-        <!-- Nav Item - Messages -->
+                <!-- Nav Item - Messages -->
 
-        <!-- Flexbox container for aligning the toasts -->
-
-
-        <!-- Nav Item - Messages -->
-        <li class="nav-item dropdown no-arrow mx-1">
-          <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fas fa-envelope fa-fw"></i>
-            <!-- Counter - Mes	sages -->
+                <!-- Flexbox container for aligning the toasts -->
 
 
-            <span class="badge badge-danger badge-counter"><?= $jml_notif ?></span>
-          </a>
-          <!-- Dropdown - Messages -->
-          <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
-            <h6 class="dropdown-header">
-              Message Center
-            </h6>
+                <!-- Nav Item - Messages -->
+                <li class="nav-item dropdown no-arrow mx-1">
+                    <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-envelope fa-fw"></i>
+                        <!-- Counter - Mes	sages -->
+
+
+                        <span class="badge badge-danger badge-counter"><?=$jml_notif?></span>
+                    </a>
+                    <!-- Dropdown - Messages -->
+                    <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                        aria-labelledby="messagesDropdown">
+                        <h6 class="dropdown-header">
+                            Message Center
+                        </h6>
 
 
 
 
-            <?php foreach ($notif as $n) : ?>
-              <?php
-              if ($n->jenis == "pemesanan" || $n->jenis == "info") {
-                $href = base_url('pencari/pemesanan');
-              } elseif ($n->jenis == 'pembayaran' || $n->jenis == 'pelunasan') {
-                $href = base_url('pencari/pembayaran');
-              }
-              if ($n->status_baca == 0) {
-                $color = 'black';
-              } else {
-                $color = 'grey';
-              }
-              ?>
-              <a class="dropdown-item d-flex align-items-center notif<?= $n->id_notifikasi ?>" data-url="<?= $href ?>" onclick="notifDibaca(<?= $n->id_notifikasi ?>)">
-                <div class="font-weight-bold">
-                  <div class="text-truncate" style="color: <?= $color ?>"><?= $n->isi_pesan ?></div>
-                  <div class="small text-gray-500"><?= $n->date ?></div>
+                        <?php foreach ($notif as $n): ?>
+                        <?php
+if ($n->jenis == "pemesanan" || $n->jenis == "info") {
+    $href = base_url('pencari/pemesanan');
+} elseif ($n->jenis == 'pembayaran' || $n->jenis == 'pelunasan') {
+    $href = base_url('pencari/pembayaran');
+}
+if ($n->status_baca == 0) {
+    $color = 'black';
+} else {
+    $color = 'grey';
+}
+?>
+                        <a class="dropdown-item d-flex align-items-center notif<?=$n->id_notifikasi?>"
+                            data-url="<?=$href?>" onclick="notifDibaca(<?=$n->id_notifikasi?>)">
+                            <div class="font-weight-bold">
+                                <div class="text-truncate" style="color: <?=$color?>"><?=$n->isi_pesan?></div>
+                                <div class="small text-gray-500"><?=$n->date?></div>
+                            </div>
+                        </a>
+                        <?php endforeach;?>
+
+
+
+
+                    </div>
+                </li>
+
+                <div class="topbar-divider d-none d-sm-block">
+
+
+
+
                 </div>
-              </a>
-            <?php endforeach; ?>
 
 
-
-
-          </div>
-        </li>
-
-        <div class="topbar-divider d-none d-sm-block">
-
-
-
-
-        </div>
-
-
-        <!-- Nav Item - User Information -->
-        <li class="nav-item dropdown no-arrow">
-          <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $nama->nama_pencari; ?></span>
-            <img class="img-profile rounded-circle" src="<?= base_url('asset_registrasi/upload_pencari/') . $nama->foto ?>">
-          </a>
-          <!-- Dropdown - User Information -->
-          <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-            <a class="dropdown-item" href="<?php echo base_url('pencari/') ?>profile">
-              <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-              Profile
-            </a>
-            <!--<a class="dropdown-item" href="<?php echo base_url('pencari/') ?>profile">
+                <!-- Nav Item - User Information -->
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?=$nama->nama_pencari;?></span>
+                        <img class="img-profile rounded-circle"
+                            src="<?=base_url('asset_registrasi/upload_pencari/') . $nama->foto?>">
+                    </a>
+                    <!-- Dropdown - User Information -->
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
+                        aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="<?php echo base_url('pencari/') ?>profile">
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Profile
+                        </a>
+                        <!--<a class="dropdown-item" href="<?php echo base_url('pencari/') ?>profile">
                   <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                   Settings
                 </a>-->
-            <!--<a class="dropdown-item" href="#">
+                        <!--<a class="dropdown-item" href="#">
                   <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                   Activity Log
                 </a>-->
-            <div class="dropdown-divider"></div>
+                        <div class="dropdown-divider"></div>
 
 
 
@@ -186,17 +212,18 @@
 
 
 
-            <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
 
-              <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-              Logout
-            </a>
-          </div>
-        </li>
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                            Logout
 
-      </ul>
+                        </a>
+                    </div>
+                </li>
+
+            </ul>
 
 
 
 
-    </nav>
+        </nav>
